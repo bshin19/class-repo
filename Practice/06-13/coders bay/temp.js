8747a8a238375a037c86ae6eddbac68f4efe8da6
Brshin <bradyshinners@gmail.com>
2018-06-16 13:57:09 -0500

    06-16 push

---
+++ Practice/06-13/coders bay/temp.js
@@ -0,0 +1,12 @@
+//JAVASCRIPT IN//
+$("#eTable").append(
+    "<tr><th scope='row'>" + numEmployees +
+    "</th>" +
+    "<td>" + eName + "</td>" +
+    "<td>" + eRole + "</td>" +
+    "<td>" + eDate + "</td>" +
+    "<td>" + eMonths + "</td>" +
+    "<td>" + eRate + "</td>" +
+    "<td>" + eEarnings + "</td>" +
+    "</tr>"
+)
